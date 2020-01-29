1380faaf981909ca0e531cf9bbd2f853e247ae57
victorjp1 <victorjp999@gmail.com>
2020-01-29 16:19:37 +0100

    Ejercicio1 Pila

--- src/main/java/com/victorlopez/tema08/pilasColas/Ejercicio1/Main.java
+++ src/main/java/com/victorlopez/tema08/pilasColas/Ejercicio1/Main.java
@@ -2,9 +2,10 @@ package com.victorlopez.tema08.pilasColas.Ejercicio1;
 
 public class Main {
     public static void main(String[] args) {
+        //Pruebas para la clase pila
         Pila p1 = new Pila(10);
-        System.out.println(p1.pop());
-        p1.push(0);
+        System.out.println(p1.pop());//Elimina y devuelve el último valor de la pila
+        p1.push(0);//Introduce un valor en la pila
         p1.push(1);
         p1.push(2);
         p1.push(3);
@@ -14,13 +15,12 @@ public class Main {
         p1.push(7);
         p1.push(8);
         p1.push(9);
-        System.out.println(p1.top());
+        System.out.println(p1.top());//Devuelve el último valor de la pila sin eliminarlo
         p1.push(10);
         p1.push(11);
         p1.push(12);
         System.out.println(p1.pop());
         p1.push(12);
-
         System.out.println(p1.toString());
     }
 }
